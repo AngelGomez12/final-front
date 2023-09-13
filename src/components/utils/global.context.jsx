966ecbaeccb2118ccdd57e051a085ce65f141bfe
AngelGome12 <gomez10828@gmail.com>
2023-09-13 03:08:11 -0300

    feat: :sparkles: change in details and home

--- src/components/utils/global.context.jsx
+++ src/components/utils/global.context.jsx
@@ -1,8 +1,9 @@
-import { createContext, useReducer, useEffect, useContext } from "react";
+import { createContext, useReducer, useContext } from "react";
 
 export const initialState = {
     theme: "",
-    data: []
+    data: [],
+    user: {},
 }
 
 export const ContextGlobal = createContext(undefined);
@@ -16,26 +17,58 @@ export const ContextProvider = ({ children }) => {
         switch (action.type) {
             case 'SET_THEME':
                 return { ...state, theme: action.payload };
+            case 'LOAD_DATA':
+                return { ...state, data: action.payload };
             case 'SET_DATA':
                 return { ...state, data: action.payload };
+            case 'LOAD_USER':
+                return { ...state, user: action.payload };
+            case 'SET_USER':
+                return { ...state, user: action.payload };
+            case 'ADD_FAV':
+                return { ...state, data: [...state.data, action.payload] };
+            case 'CLEAR_DATA':
+                return { ...state, data: [] };
             default:
                 return state;
         }
     }
 
-    // const loadData = async () => {
-    //     try {
-    //         const response = await fetch('https://jsonplaceholder.typicode.com/users');
-    //         const data = await response.json();
-    //         dispatch({ type: 'SET_DATA', payload: data });
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // }
+    const loadData = async () => {
+        dispatch({ type: 'LOAD_DATA', payload: state.data })
+        try {
+            const response = await fetch('https://jsonplaceholder.typicode.com/users');
+            const data = await response.json();
+            dispatch({ type: 'SET_DATA', payload: data });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const getUserById = async (id) => {
+        dispatch({ type: 'LOAD_USER', payload: state.user })
+        try {
+            const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+            const user = await response.json();
+            dispatch({ type: 'SET_USER', payload: user });
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     return (
-        <ContextGlobal.Provider value={{ state, dispatch }}>
+        <ContextGlobal.Provider value={{ state, dispatch, loadData, getUserById }}>
             {children}
         </ContextGlobal.Provider>
     );
 };
+
+export const useGlobalContext = () => {
+    return useContext(ContextGlobal);
+}
+
+export const useLocalStorage = (key, initialValue) => {
+      const dentistFav = localStorage.getItem(`${key}`) || '';
+        const newDentistFav = dentistFav ? `${dentistFav},${initialValue}` : initialValue;
+        localStorage.setItem('dentistFav', newDentistFav);
+}
--- src/pages/Favs.jsx
+++ src/pages/Favs.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
 
 export const Favs = () => {
+
+    
     return (
         <div>Favs</div>
     )
